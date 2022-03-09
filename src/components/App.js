d6aa1bc0579a133e78804fedc3a218db31abc629
anjali7528 <anjaligandhi248@gmail.com>
2022-03-10 01:20:40 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -45,39 +45,40 @@ class App extends React.Component {
   const {list, favourites, showFavourites} = movies; //{movies: {}, search :{}}
   console.log('RENDER', this.props.store.getState());
     
-  const displayMovies = showFavourites ? favourites : list;
-  return(
-    <StoreContext.Consumer>
-       {(store) => {
-         return (
-          <div className="App">
-            <Navbar dispatch={this.props.store.dispatch} search={search}/>
-            <div className="main">
-              <div className="tabs">
-                <div className={`tab ${showFavourites ? '': 'active-tabs'}`} onClick={() => this.onChangeTab(false)}>Movies</div>
-                <div className ={`tab ${showFavourites ? 'active-tabs': ''}`} onClick={() => this.onChangeTab(true)}>Favourites</div>
-                </div>
-                <div className="list">
-                {displayMovies.map((movie, index) =>(
-                  <MovieCard 
-                  movie ={movie}
-                  key={`movies-${index}`} 
-                  dispatch={this.props.store.dispatch} 
-                  isFavourite= {this.isMovieFavourite(movie)}/>
-                ))}
-                </div>
-                {displayMovies.length === 0 ? <div className="no-movies">No movies to display !</div>: null}
-            </div>
+  const displayMovies = showFavourites ? favourites : list;  
+  return (
+    <div className="App">
+      <Navbar dispatch={this.props.store.dispatch} search={search}/>
+      <div className="main">
+        <div className="tabs">
+          <div className={`tab ${showFavourites ? '': 'active-tabs'}`} onClick={() => this.onChangeTab(false)}>Movies</div>
+          <div className ={`tab ${showFavourites ? 'active-tabs': ''}`} onClick={() => this.onChangeTab(true)}>Favourites</div>
           </div>
-        );
-       }
-       }
-    </StoreContext.Consumer>
-  )
-  
-  
+          <div className="list">
+          {displayMovies.map((movie, index) =>(
+            <MovieCard 
+            movie ={movie}
+            key={`movies-${index}`} 
+            dispatch={this.props.store.dispatch} 
+            isFavourite= {this.isMovieFavourite(movie)}/>
+          ))}
+          </div>
+          {displayMovies.length === 0 ? <div className="no-movies">No movies to display !</div>: null}
+      </div>
+    </div>
+  );
 
  }
+}
+
+class AppWraper extends React.Component{
+  render(){
+    return(
+      <StoreContext.Consumer>
+        {(store) => <App store={store}/>}
+      </StoreContext.Consumer>
+    );
+  }
 }
 
 export default App;
